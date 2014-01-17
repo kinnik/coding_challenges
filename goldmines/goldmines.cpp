174a6e735b86e6c9f2eb04e9c0638807e45fbacd
kinnik <northislandking@gmail.com>
2014-01-17 17:30:30 +0000

    stdin read is now done using scanf; this improved the performance

--- goldmines/goldmines.cpp
+++ goldmines/goldmines.cpp
@@ -1,115 +1,73 @@
 #include <stdio.h>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
 #include <time.h>
 
 using namespace std;
 
 int main()
 {
+  // declarations & initialisations
   time_t start = time(NULL);
   int sec;
 
-  string line;
-  unsigned short line_num = 1;
-  unsigned short num_rows, num_cols;
-  unsigned short query_anchor = 0;
+  unsigned short num_rows, num_cols = 0;
   unsigned short num_queries = 0;
+  unsigned short num_executions = 0;
   
   unsigned int* gold_data;
   unsigned int** gold;
-  vector<unsigned long> results;
 
-  // http://stackoverflow.com/questions/9371238/why-is-reading-lines-from-stdin-much-slower-in-c-than-python
-  cin.sync_with_stdio(false);
-  cout.sync_with_stdio(false);
+  register unsigned short row_lo = 0;
+  register unsigned short col_lo = 0;
+  register unsigned short row_hi = 0;
+  register unsigned short col_hi = 0;
 
-  while (getline(cin, line))
-  {
-    stringstream ss(line);
-    ss.sync_with_stdio(false);
+  // read rows & columns
+  scanf("%hu%hu", &num_rows, &num_cols);
 
-    if (line_num == 1)
-    {
+  // declare and initialise the contriner
+  gold_data = new unsigned int[num_rows * num_cols];
+  gold = new unsigned int*[num_rows];
 
-      ss >> num_rows >> num_cols;
-      
-      gold_data = new unsigned int[num_rows * num_cols];
-      gold = new unsigned int*[num_rows];
+  for (unsigned short a = 0; a < num_rows; ++a)
+    gold[a] = gold_data + num_cols * a;
 
-      for (unsigned short a = 0; a < num_rows; ++a)
-        gold[a] = gold_data + num_cols * a;
-
-      query_anchor = num_rows + 2;
+  // read in the gold
+  for (unsigned short i=0; i<num_rows; ++i) {
+    for (unsigned short j=0; j<num_cols; ++j) {
+      scanf("%u", &gold[i][j]);
     }
-    else if (line_num < query_anchor)
-    {
-        string field;
-
-        unsigned short j = 0;
-        while(getline(ss, field, ' '))
-        {
-            stringstream strstm(field);
-            strstm.sync_with_stdio(false);
-
-            unsigned int g;
-            strstm >> g;
-
-            gold[line_num-2][j] = g;
-            ++j;
-        }
-    }
-    else if (line_num == query_anchor)
-    {
-      ss >> num_queries;
-      results.resize(num_queries);
-    }
-    else
-    { 
-      unsigned short row_lo = 0;
-      unsigned short col_lo = 0;
-      unsigned short row_hi = 0;
-      unsigned short col_hi = 0;
-
-      ss >> row_lo >> col_lo >> row_hi >> col_hi;
-
-      //printf ("%d %d %d %d", row_lo, col_lo, row_hi, col_hi);
-
-      // remember, the grid is zero-indexed
-      --row_lo;
-      --col_lo;
-      --row_hi;
-      --col_hi;
-
-      unsigned long total_gold = 0;
-
-      for (unsigned short r=row_lo; r<= row_hi; ++r)
-      {
-        for (unsigned short c=col_lo; c<= col_hi; ++c) 
-        {
-          total_gold += gold[r][c];
-        }
-      }
-      results[line_num-num_rows-3] = total_gold;
-      // fprintf(stdout, "%lu\n", total_gold);
-      // fflush(stdout);
-      //cout << total_gold << endl;
-    }
-    ++line_num;
   }
-  delete[] gold;
-  delete[] gold_data;
 
-  for (vector<unsigned long>::const_iterator i = results.begin(); i != results.end(); ++i)
-   {  
-      cout << *i << endl;
-  //     fprintf(stdout, "%llu\n", *i);
-  //     fflush(stdout);
-   }
+  // read the number of queries and size the results vector
+  scanf("%hu", &num_queries);
+  //results.resize(num_queries);
+
+  while (num_executions < num_queries)
+  {
+    // read and process the queries
+    scanf("%hu%hu%hu%hu", &row_lo, &col_lo, &row_hi, &col_hi);
+      // remember, the grid is zero-indexed
+    --row_lo;
+    --col_lo;
+    --row_hi;
+    --col_hi;
+
+    register unsigned long total_gold = 0;
+
+    for (unsigned short r=row_lo; r<= row_hi; ++r)
+    {
+      for (unsigned short c=col_lo; c<= col_hi; ++c) 
+      {
+         total_gold += gold[r][c];
+      }
+    }
+    fprintf(stdout, "%lu\n", total_gold);
+    fflush(stdout);
+    ++num_executions;
+  }
+
   sec = (int) time(NULL) - start;
-  cout << sec << "seconds" << endl;
+  fprintf(stdout, "%d seconds\n", sec);
 
   return 0;
 }
