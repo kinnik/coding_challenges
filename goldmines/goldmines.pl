c3aa8b6757d60a6e0fe4c8a0696ab93691e1c1cd
kinnik <northislandking@gmail.com>
2014-01-19 17:43:31 +0000

    Perl implementation using the more-efficient algorithm

--- goldmines/goldmines.pl
+++ goldmines/goldmines.pl
@@ -2,74 +2,64 @@
 
 $|=1;
 
-my $index = 0;
-my @dim;
-my @grid;
+use Benchmark;
+use Data::Dumper;
+
+my $start_time;
+
+my ($num_rows, $num_cols);
+my @sum_grid;
 my @results;
 
-while (<>)
-{
-  chomp;
-  if ($index == 0) 
-  {
-    @dim = split / /, $_;
-    
-    if ($dim[0] < 1 or $dim[0] > 1000)
-    {
-      exit -1;
-    }
-    if ($dim[1] < 1 or $dim[1] > 1000)
-    {
-      exit -1;
-    }
-    $index += 1;
-    next;
-  }
+my @input = <STDIN>;
+chomp @input;
 
-  if ($index <= $dim[0]) 
-  {
-    my @row_gold = split / /, $_, $dim[1];
-    push @{$grid[$index-1]}, @row_gold;
-  }
-  elsif ($index == $dim[0] + 1) 
-  {
-    # fix the results array size
-    $#results = $_ - 1;
-  }
-  else
-  {
-    my @query = split / /, $_;
+$start_time = new Benchmark;
 
-    if ($query[0] < 1 or $query[0] > $dim[0])
-    {
-      exit -1;
-    }
-    if ($query[0] > $query[2]) { exit -1; }
+foreach my $line_num(0..$#input) {
 
-    if ($query[1] < 1 or $query[1] > $dim[1])
-    {
-      exit -1;
-    }
-    if ($query[1] > $query[3]) { exit -1; }
-
-    my $row_lo = $query[0] - 1;
-    my $row_hi = $query[2] - 1;
-    my $col_lo = $query[1] - 1;
-    my $col_hi = $query[3] - 1;
+  if ($line_num == 0) {
+    ($num_rows, $num_cols) = split m/\s/, $input[$line_num], 2;
+  } elsif ($line_num <= $num_rows) {
+    my @row_gold = split m/\s/, $input[$line_num], $num_cols;
 
     my $sum = 0;
+    my @accumulator;
 
-    foreach my $c($col_lo..$col_hi) {
-      foreach my $r($row_lo..$row_hi) {
-        $sum += $grid[$r][$c]
+    foreach my $index(0..$#row_gold) {
+      $sum += $row_gold[$index];
+
+      if ($line_num == 1) {
+        push @accumulator, $sum;
+      } else {
+        push @accumulator, $sum_grid[-1][$index] + $sum
       }
     }
-    #print STDOUT "$sum\n";
-    $results[$index-$dim[0]-2] = $sum
+    push @sum_grid, [@accumulator];
+  } elsif ($line_num == $num_rows + 1) {
+    # fix the results array size
+    # $#results = $line_num - 1;
+  } else {
+    my ($x1, $y1, $x2, $y2) = map { $_ - 1 } split m/\s/, $input[$line_num], 4;
+
+    my $sum = $sum_grid[$x2][$y2];
+    if ($x1 != 0 && $x2 != 0) {
+      $sum -= $sum_grid[$x1-1][$y2]
+    }
+    if ($y1 != 0) {
+      $sum -= $sum_grid[$x2][$y1-1]
+    }
+    if ($x1 != 0 && $y1 != 0) {
+      $sum += $sum_grid[$x1-1][$y1-1]
+    }
+
+    push @results, $sum;
   }
 
-  $index += 1;
-  next;
 }
 print STDOUT join "\n", @results;
+print STDOUT "\n";
 
+my $end_time = new Benchmark;
+my $difference = timediff($end_time, $start_time);
+print STDOUT "It took ", timestr($difference), "\n";
