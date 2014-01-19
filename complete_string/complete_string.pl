f7c6dd06c8bafaccba571f170c49d8c152bf305b
kinnik <northislandking@gmail.com>
2014-01-19 21:39:21 +0000

    Perl implmentation

---
+++ complete_string/complete_string.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl -w
+
+use strict;
+use warnings;
+
+use constant YES => "YES";
+use constant NO => "NO";
+
+my @results;
+
+my @input = <STDIN>;
+chomp @input;
+
+
+my $num_strings = $input[0];
+
+foreach (1..$num_strings) {
+
+  my %h = map { if (m/[a-z]/) { ($_, undef) } } split //, $input[$_];
+
+  if (scalar keys %h == 26) { 
+    push @results, YES;
+  } else {
+    push @results, NO;
+  }
+
+}
+
+print STDOUT join "\n", @results;
+print STDOUT "\n";
