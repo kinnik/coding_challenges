c72e9716af2604d06a2ba34ed26ae21ce58d9aef
kinnik <northislandking@gmail.com>
2014-01-19 15:12:59 +0000

    took three and a half seconds off

--- goldmines/goldmines.rb
+++ goldmines/goldmines.rb
@@ -9,85 +9,74 @@ results = Array.new
 Benchmark.bmbm do |benchmark|
 
   benchmark.report ("the whole thing") do
-  ARGF.readlines.each_with_index do |line, line_num|
+    ARGF.readlines.each_with_index do |line, line_num|
 
-      if line_num == 0
-        dim = line.scan(/\d+/).map(&:to_i)
-        sum_grid = Array.new(dim[0])
-      elsif line_num <= dim[0]
-        
-          vacc = Array.new()
-          j = 0
-          line.scan(/\d+/).map(&:to_i).reduce(0) do |acc, current|
-            sum = acc + current
-
-            if (line_num == 1)
-              vacc.push(sum)
-            else
-              vacc.push(sum_grid[line_num-2][j] + sum)
-            end
-
-            j += 1
-            sum
-          end
-          sum_grid[line_num-1] = vacc
-
-      elsif line_num == dim[0] + 1
-        #results = Array.new(line.to_i)
-      else
-
-          # -1 because the grid is zero-indexed
-          query = line.scan(/\d+/).map{ |x| x.to_i - 1 }
-
-          first_subtraction = 0
-          second_subtraction = 0
-          all_sum = 0
-
-          # for readibility
-          x1 = query[0]
-          y1 = query[1]
-          x2 = query[2]
-          y2 = query[3]
-
-
-          left_bound = [0,0]
-          top_bound = [0,0]
-          vert_bound = [0,0]
-
-          left_bound[0] = x2
-          left_bound[1] = y1 - 1
-
-          top_bound[0] = x1 - 1
-          top_bound[1] = y2
+        if line_num == 0
+          dim = line.scan(/\d+/).map(&:to_i)
           
-          vert_bound[0] = x1 - 1
-          vert_bound[1] = y1 - 1
+        elsif line_num <= dim[0]
+          
+            accumulator = Array.new()
+            j = 0
+            line.scan(/\d+/).map(&:to_i).reduce(0) do |acc, current|
+              sum = acc + current
 
-          # let's do this
-          if (top_bound[0] != -1)
-            if (x2 != 0)
-              first_subtraction = sum_grid[x2][y2] - sum_grid[top_bound[0]][top_bound[1]]
+              if (line_num == 1)
+                accumulator.push(sum)
+              else
+                accumulator.push(sum_grid[line_num-2][j] + sum)
+              end
+
+              j += 1
+              sum
             end
-          else
-            first_subtraction = sum_grid[x2][y2]
-          end
+            sum_grid.push(accumulator)
 
-          if (left_bound[1] != -1)
-            if (y1 != 0)
-              second_subtraction = sum_grid[left_bound[0]][left_bound[1]]
+        elsif line_num == dim[0] + 1
+          #results = Array.new(line.to_i)
+        else
+
+            # -1 because the grid is zero-indexed
+            query = line.scan(/\d+/).map{ |x| x.to_i - 1 }
+
+            first_subtraction = 0
+            second_subtraction = 0
+            all_sum = 0
+
+            # for readibility
+            x1 = query[0]
+            y1 = query[1]
+            x2 = query[2]
+            y2 = query[3]
+
+            col_bound = y1 - 1
+            row_bound = x1 - 1
+
+            # let's do this
+            if (row_bound != -1)
+              if (x2 != 0)
+                first_subtraction = sum_grid[x2][y2] - sum_grid[row_bound][y2]
+              end
+            else
+              first_subtraction = sum_grid[x2][y2]
             end
-          end
 
-          if (vert_bound[0] != -1 && vert_bound[1] != -1)
-            second_subtraction -= sum_grid[vert_bound[0]][vert_bound[1]]
-          end
+            if (col_bound != -1)
+              if (y1 != 0)
+                second_subtraction = sum_grid[x2][col_bound]
+              end
+            end
 
-          all_sum = first_subtraction - second_subtraction
+            if (row_bound != -1 && col_bound != -1)
+              second_subtraction -= sum_grid[row_bound][col_bound]
+            end
 
-          results.push(all_sum)
+            all_sum = first_subtraction - second_subtraction
 
-      end
-  end
+            results.push(all_sum)
+
+        end
+    end
 
 
     puts results
