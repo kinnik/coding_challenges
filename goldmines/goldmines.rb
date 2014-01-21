43f88e133629cb84bad6e7eb3c9253231845f33a
kinnik <northislandking@gmail.com>
2014-01-21 21:19:11 +0000

    got rid of the acuumulator array

--- goldmines/goldmines.rb
+++ goldmines/goldmines.rb
@@ -2,53 +2,54 @@
 
 require 'benchmark'
 
-dim = []
-sum_grid = []
-results = []
-
 Benchmark.bmbm do |benchmark|
 
   benchmark.report ("the whole thing") do
+  
+    sum_grid = []
+    results = []
+    num_rows, num_cols = 0
+
     ARGF.readlines.each_with_index do |line, line_num|
 
         if line_num == 0
-          dim = line.scan(/\d+/).map(&:to_i)
-        elsif line_num <= dim[0]
-            accumulator = []
+          num_rows, num_cols = line.scan(/\d+/).map(&:to_i)
+          sum_grid = Array.new(num_rows) { Array.new(num_cols)}
+        elsif line_num <= num_rows
+
             j = 0
             line.scan(/\d+/).map(&:to_i).reduce(0) do |acc, current|
               sum = acc + current
 
               if (line_num == 1)
-                accumulator.push(sum)
+                sum_grid[line_num-1][j] = sum
               else
-                accumulator.push(sum_grid.last[j] + sum)
+                sum_grid[line_num-1][j] = sum_grid[line_num-2][j] + sum
               end
 
               j += 1
               sum
             end
-            sum_grid.push(accumulator)
 
-        elsif line_num == dim[0] + 1
+        elsif line_num == num_rows + 1
           #results = Array.new(line.to_i)
         else
 
             # -1 because the grid is zero-indexed
-            query = line.scan(/\d+/).map{ |x| x.to_i - 1 }
+            x1, y1, x2, y2 = line.scan(/\d+/).map{ |x| x.to_i - 1 }
 
             # let's do this
-            all_sum = sum_grid[query[2]][query[3]]
-            if (query[0] != 0 && query[2] != 0)
-                all_sum -= sum_grid[query[0]-1][query[3]]
+            all_sum = sum_grid[x2][y2]
+            if (x1 != 0 && x2 != 0)
+                all_sum -= sum_grid[x1-1][y2]
             end
 
-            if (query[1] != 0)
-                all_sum -= sum_grid[query[2]][query[1]-1]
+            if (y1 != 0)
+                all_sum -= sum_grid[x2][y1-1]
             end
 
-            if (query[0] != 0 && query[1] != 0)
-              all_sum += sum_grid[query[0]-1][query[1]-1]
+            if (x1 != 0 && y1 != 0)
+              all_sum += sum_grid[x1-1][y1-1]
             end
 
             results.push(all_sum)
