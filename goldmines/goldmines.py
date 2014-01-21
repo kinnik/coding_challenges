c52fbb71fe1bd04db40e0a6a853428eac29387b3
kinnik <northislandking@gmail.com>
2014-01-21 20:29:38 +0000

    got rid of the accumulator array; wrapped all the code into a function which makes it run faster; the memory usage has gone up slightly

--- goldmines/goldmines.py
+++ goldmines/goldmines.py
@@ -2,48 +2,47 @@
 
 import sys
 
-sum_grid = []
-results = []
-input_text = sys.stdin.readlines()
+def run():
+  results = []
+  input_text = sys.stdin.readlines()
+
+  num_rows, num_cols = map(int, input_text[0].split())
+  sum_grid = [[None] * num_cols for r in range(num_rows)]
 
 
-num_rows, num_cols = map(int, input_text[0].split())
+  for i in range(0, num_rows):
+    sum = 0;
 
-for line_num in range(1,num_rows+1):
+    row_gold = map(int, input_text[i+1].split())
 
-  row_gold = map(int, input_text[line_num].split())
+    for j in range(0, num_cols):
+      sum += row_gold[j];
 
-  sum = 0;
-  accumulator = [];
-
-  for i in range(0, len(row_gold)):
-    sum += row_gold[i]
-    
-    if line_num == 1: #first line of the gold in the grid
-      accumulator.append(sum)
-    else:
-      accumulator.append(sum_grid[-1][i] + sum)
-
-  sum_grid.append(accumulator)
+      if i == 0: #first line of the gold in the grid
+        sum_grid[i][j] = sum
+      else:
+        sum_grid[i][j] = sum_grid[i-1][j] + sum
 
 
-num_queries = int(input_text[num_rows + 1])
+  num_queries = int(input_text[num_rows + 1])
 
-for q in range(num_rows+2,num_rows+2+num_queries): #python's range exclude the upper bound
-  x1, y1, x2, y2 = map(lambda i: int(i) - 1, input_text[q].split())
+  for q in range(num_rows+2,num_rows+2+num_queries): #python's range exclude the upper bound
+    x1, y1, x2, y2 = map(lambda i: int(i) - 1, input_text[q].split())
 
-  sum = sum_grid[x2][y2]
+    sum = sum_grid[x2][y2]
 
-  if (x1 != 0 and x2 != 0):
-    sum -= sum_grid[x1-1][y2]
+    if (x1 != 0 and x2 != 0):
+      sum -= sum_grid[x1-1][y2]
 
-  if (y1 != 0):
-    sum -= sum_grid[x2][y1-1]
+    if (y1 != 0):
+      sum -= sum_grid[x2][y1-1]
 
-  if (x1 != 0 and y1 != 0):
-    sum += sum_grid[x1-1][y1-1]
+    if (x1 != 0 and y1 != 0):
+      sum += sum_grid[x1-1][y1-1]
 
-  results.append(sum)
+    results.append(sum)
 
-for r in results:
-  print r
+  for r in results:
+    print r
+
+run()
