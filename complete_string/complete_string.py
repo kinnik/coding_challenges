891301f4d8dcf34c7f2726771ef07269e3a1b802
kinnik <northislandking@gmail.com>
2015-05-06 17:56:08 +0100

    minor improvements to complete_string.py

--- complete_string/complete_string.py
+++ complete_string/complete_string.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
+
+YES = "YES"
+NO = "NO"
 results = []
 num_strings = int(raw_input())
 
 for i in range(num_strings):
-  uniq_chars = set(list(raw_input()))
+  uniq_chars = {x for x in raw_input() if x in 'abcdefghijklmnopqrstuvwxyz'}
 
   if len(uniq_chars) == 26:
-    results.append("YES")
+    results.append(YES)
   else:
-    results.append("NO")
+    results.append(NO)
 
 for r in results:
   print r
-
